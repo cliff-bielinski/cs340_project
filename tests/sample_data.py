b134822394f98c2f2d0a60c4f1624783f40115ff
Clifford Bielinski <bielinsc@flip1.engr.oregonstate.edu>
2022-05-03 16:47:23 -0700

    sample_data done

--- tests/sample_data.py
+++ tests/sample_data.py
@@ -81,3 +81,155 @@ pokemon = [
     "trainer": "Ash Ketchum"
   },
 ]
+
+trainers = [
+  {
+    "trainer_id": 1,
+    "name": "Ash Ketchum",
+    "birthdate": "1996-12-01",
+    "gender": "Male"
+  },
+  {
+    "trainer_id": 2,
+    "name": "Misty Williams",
+    "birthdate": "1994-02-14",
+    "gender": "Female"
+  },
+  {
+    "trainer_id": 3,
+    "name": "Gary Oak",
+    "birthdate": "1996-04-12",
+    "gender": "Male"
+  },
+  {
+    "trainer_id": 4,
+    "name": "Brock Harrison",
+    "birthdate": "1992-06-17",
+    "gender": "Male"
+  },
+  {
+    "trainer_id": 5,
+    "name": "James Kojiro",
+    "birthdate": "1988-07-05",
+    "gender": "Male"
+  }
+]
+
+stadiums = [
+  {
+    "stadium_id": 1,
+    "name": "Pewter Gym",
+    "location": "Pewter City"
+  },
+  {
+    "stadium_id": 2,
+    "name": "Vermillion Gym",
+    "location": "Vermillion City"
+  },
+  {
+    "stadium_id": 3,
+    "name": "Fuchsia Gym",
+    "location": "Fuchsia City"
+  },
+  {
+    "stadium_id": 4,
+    "name": "Cinnabar Gym",
+    "location": "Cinnabar Island"
+  },
+  {
+    "stadium_id": 5,
+    "name": "Cerulean Gym",
+    "location": "Cerulean City"
+  }
+]
+
+battles = [
+  {
+    "battle_id": 1,
+    "date": "2022-02-01",
+    "location": "Cerulean Gym",
+    "winner": "Ash Ketchum",
+    "loser": "Misty Williams"
+  },
+  {
+    "battle_id": 2,
+    "date": "2022-03-16",
+    "location": "Pewter Gym",
+    "winner": "Ash Ketchum",
+    "loser": "Brock Harrison"
+  },
+  {
+    "battle_id": 3,
+    "date": "2022-03-16",
+    "location": "Cinnabar Gym",
+    "winner": "Gary Oak",
+    "loser": "James Kojiro"
+  },
+  {
+    "battle_id": 4,
+    "date": "2022-03-26",
+    "location": "Pewter Gym",
+    "winner": "Gary Oak",
+    "loser": "Brock Williams"
+  },
+  {
+    "battle_id": 5,
+    "date": "2022-04-13",
+    "location": "Vermillion Gym",
+    "winner": "James Kojiro",
+    "loser": "Gary Oak"
+  }
+]
+
+pokemon_battles = [
+  {
+    "pokebattle_id": 1,
+    "stadium": "Cerulean Gym",
+    "date": "2022-02-01",
+    "nickname": "Zaps",
+    "species": "Pikachu",
+    "trainer": "Ash Ketchum",
+    "winner": True,
+    "knocked_out": False
+  },
+  {
+    "pokebattle_id": 2,
+    "stadium": "Pewter Gym",
+    "date": "2022-03-16",
+    "nickname": "Zaps",
+    "species": "Pikachu",
+    "trainer": "Ash Ketchum",
+    "winner": True,
+    "knocked_out": False
+  },
+  {
+    "pokebattle_id": 3,
+    "stadium": "Cerulean Gym",
+    "date": "2022-02-01",
+    "nickname": "Bulba",
+    "species": "Bulbasaur",
+    "trainer": "Misty Williams",
+    "winner": False,
+    "knocked_out": True
+  },
+  {
+    "pokebattle_id": 4,
+    "stadium": "Cinnabar Gym",
+    "date": "2022-03-16",
+    "nickname": None,
+    "species": "Pikachu",
+    "trainer": "Gary Oak",
+    "winner": True,
+    "knocked_out": False
+  },
+  {
+    "pokebattle_id": 5,
+    "stadium": "Pewter Gym",
+    "date": "2022-03-16",
+    "nickname": "Beaks",
+    "species": "Pidgey",
+    "trainer": "Brock Harrison",
+    "winner": False,
+    "knocked_out": True
+  },
+]
